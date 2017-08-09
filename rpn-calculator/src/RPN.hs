f06480c2f1fd338f48cb9d97dc00680241c2dae0
Matthias Heinzel <mheinzel@posteo.de>
2017-08-09 19:10:05 +0200

    Add function prototypes

--- rpn-calculator/src/RPN.hs
+++ rpn-calculator/src/RPN.hs
@@ -1,5 +1,16 @@
-module RPN where
+module RPN (BinaryOperator, parseBinaryOperator, applyBinaryOperator, evaluate) where
 
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+type BinaryOperator = String  -- TODO: think of something better
+
+
+parseBinaryOperator :: String -> Maybe BinaryOperator
+parseBinaryOperator = undefined
+
+
+applyBinaryOperator :: BinaryOperator -> Integer -> Integer -> Integer
+applyBinaryOperator = undefined
+
+
+evaluate :: String -> Either String Integer
+evaluate = undefined
